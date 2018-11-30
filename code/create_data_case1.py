142fee7ef1ba128a65382cf546cdbad257cdcc0a
Deepak Gupta <guptadeepak2806@gmail.com>
2018-11-30 18:40:20 +0530

    Created first sets of input and target and trained CNN

--- code/create_data_case1.py
+++ code/create_data_case1.py
@@ -14,13 +14,56 @@ synthetic data stored in data/raw/cseg_paper_fault_dyke_fold_model
 import numpy as np
 import matplotlib.pyplot as plt
 import glob
+import noise_adder as nadd
+import random
 
 count = 0
+n_duplicates = 1 #number of noisy duplicates of each image
 
-list_of_files = glob.glob('../data/raw/cseg_paper_fault_dyke_fold_model/*')
+sigma_fact_range = [0, 0.2]
+mu_fact_range = [0 , 0.2]
+
+obj_noise = nadd.NoiseAdder()
+
+# Defining the conotrlling parameters for noise addition
+
+list_of_files = glob.glob('../data/raw/cseg_fault_dyke_fold_model/*')
+input_path = '../data/processed/cseg_fault_dyke_fold_model_50/input/'
+output_path = '../data/processed/cseg_fault_dyke_fold_model_50/output/'
+
+noisefree_indices = [] # to store the indices of files that contain noisefree data
+
 for file in list_of_files:
     
     # load the data from the npy file
     raw_data = np.load(file)
-    print(count)
+    raw_data = raw_data[::4, ::4]
+    
+    # add the noisefree file into the dataset
+    fname_i = input_path + 'data_' + str(count) + '.npy'
+    fname_o = output_path + 'data_' + str(count) + '.npy'
+    np.save(fname_i, raw_data)
+    np.save(fname_o, raw_data)
     count += 1
+        
+    # Creating n_duplicates with noise
+    for i in range(n_duplicates):
+                    
+        # generate a random number to choose which noise to add
+        randno = random.randint(1, 1)
+        if randno <= 2:
+            # Adding normal noise
+            sigma_fact = sigma_fact_range[0] + (sigma_fact_range[1] - sigma_fact_range[0]) * random.uniform(0, 1)
+            mu_fact = mu_fact_range[0] + (mu_fact_range[1] - mu_fact_range[0]) * random.uniform(0, 1)
+            img_new = obj_noise.add_normal_noise(raw_data, sigma_fact, mu_fact)
+            #plt.imshow(img_new)
+            fname_i = input_path + 'data_' + str(count) + '.npy'
+            fname_o = output_path + 'data_' + str(count) + '.npy'
+            np.save(fname_i, img_new)
+            np.save(fname_o, raw_data)
+            
+            count = count + 1
+            print(count)
+        
+    
+
