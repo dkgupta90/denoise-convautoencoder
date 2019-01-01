58a2fb665dd146e0bdaa935da968cf3321de9549
Deepak Gupta <guptadeepak2806@gmail.com>
2019-01-01 15:54:35 +0530

    code completed for Udacity capstone project v1

--- code/create_data_case1.py
+++ code/create_data_case1.py
@@ -18,7 +18,7 @@ import noise_adder as nadd
 import random
 
 count = 0
-n_duplicates = 3 #number of noisy duplicates of each image
+n_duplicates = 2 #number of noisy duplicates of each image
 
 sigma_fact_range = [0, 0.2]
 mu_fact_range = [0 , 0.2]
@@ -27,9 +27,9 @@ obj_noise = nadd.NoiseAdder()
 
 # Defining the conotrlling parameters for noise addition
 
-list_of_files = glob.glob('../data/raw/cseg_fault_dyke_fold_model/*')
-input_path = '../data/processed/cseg_fault_dyke_fold_model_50_2/input/'
-output_path = '../data/processed/cseg_fault_dyke_fold_model_50_2/output/'
+list_of_files = glob.glob('../data/raw/2D_Seismic_Alaska/slices/*')
+input_path = '../data/processed/2D_Seismic_Alaska/input/'
+output_path = '../data/processed/2D_Seismic_Alaska/output/'
 
 noisefree_indices = [] # to store the indices of files that contain noisefree data
 
@@ -37,7 +37,7 @@ for file in list_of_files:
     
     # load the data from the npy file
     raw_data = np.load(file)
-    raw_data = raw_data[::4, ::4]
+    #raw_data = raw_data[::4, ::4]
     
     # add the noisefree file into the dataset
     fname_i = input_path + 'data_' + str(count) + '.npy'
@@ -56,7 +56,6 @@ for file in list_of_files:
             sigma_fact = sigma_fact_range[0] + (sigma_fact_range[1] - sigma_fact_range[0]) * random.uniform(0, 1)
             mu_fact = mu_fact_range[0] + (mu_fact_range[1] - mu_fact_range[0]) * random.uniform(0, 1)
             img_new = obj_noise.add_normal_noise(raw_data, sigma_fact, mu_fact)
-            #plt.imshow(img_new)
             fname_i = input_path + 'data_' + str(count) + '.npy'
             fname_o = output_path + 'data_' + str(count) + '.npy'
             np.save(fname_i, img_new)
@@ -65,5 +64,22 @@ for file in list_of_files:
             count = count + 1
             print(count)
         
-    
+#    # Creating n_duplicates with noise
+#    for i in range(n_duplicates):
+#                    
+#        # generate a random number to choose which noise to add
+#        randno = random.randint(1, 1)
+#        if randno <= 2:
+#            # Adding normal noise
+#            sigma_fact = sigma_fact_range[0] + (sigma_fact_range[1] - sigma_fact_range[0]) * random.uniform(0, 1)
+#            mu_fact = mu_fact_range[0] + (mu_fact_range[1] - mu_fact_range[0]) * random.uniform(0, 1)
+#            img_new = obj_noise.add_uniform_noise(raw_data, sigma_fact, mu_fact)
+#            #plt.imshow(img_new)
+#            fname_i = input_path + 'data_' + str(count) + '.npy'
+#            fname_o = output_path + 'data_' + str(count) + '.npy'
+#            np.save(fname_i, img_new)
+#            np.save(fname_o, raw_data)
+#            
+#            count = count + 1
+#            print(count)
 
--- code/noise_adder.py
+++ code/noise_adder.py
@@ -34,6 +34,27 @@ class NoiseAdder:
         #sys.exit(0)
         noise[noise<0] = 0
         img = img + noise
+        #plt.imshow(img)
         return img
         
+    def add_uniform_noise(self, img, sigma_fact, mu_fact):
+        '''
+        This function adds Gaussian noise to the image
         
+        '''
+        amax_noise = np.max(img)
+        sigma = amax_noise * sigma_fact
+        mu = amax_noise * mu_fact
+        # Gaussian noise with sigma and mu specified above
+        noise = np.random.uniform(0, sigma, img.shape)
+        
+
+#        count, bins, ignored = plt.hist(noise, 30, density=True)
+#        plt.plot(bins, 1/(sigma * np.sqrt(2 * np.pi)) *
+#             np.exp( - (bins - mu)**2 / (2 * sigma**2) ),
+#             linewidth=2, color='r')
+#        plt.show()
+        #sys.exit(0)
+        noise[noise<0] = 0
+        img = img + noise
+        return img        
